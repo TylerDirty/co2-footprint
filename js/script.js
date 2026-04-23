bd79cc1b36def15e2f5c6e49b2260e71f2ed5be9
Callum McSkimming <callum.mcskimming@iu-study.org>
2026-04-23 10:47:32 +0200

    Grundstruktur der JavaScript-Datenbasis und Tabellenreferenz erstellt

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let emissionsdaten = [
+  {land: "Deutschland", unternehmen: "Fuchsbau GmbB", emissionen: 12000 },
+  {land: "Frankreich", unternehmen: "Énergie Verte SAS", emissionen: 8700 },
+  {land: "Großbritannien", unternehmen: "Greenfield Energy Ltd", emissionen: 15400 },
+];
+let tabellenkoerper = document.querySelector("tbody");
+
+function zeigeTabelle(daten) {
+  tabellenkoerper.innerHTML += `
+      <tr>
+        <td>${datensatz.land}</td>
+        <td>${datensatz.unternehmen}</td>
+        <td>${datensatz.emisssionen}</td>
+      </tr>
+    `;
+  }
+}
